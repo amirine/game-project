8ca71acbddb413d350576546cd86b7a88bc1c4a6
amirine <zhuk.irine@gmail.com>
2021-11-23 19:57:41 +0300

    Requirement.txt added, new base template implemented

--- game_project/settings.py
+++ game_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game.apps.GameConfig' # или лучше просто 'game'?
+    'game',
 ]
 
 MIDDLEWARE = [
